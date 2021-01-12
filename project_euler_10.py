1ef1b86852b10a448ef92571f2165c924d56775a
Matheus Guimarães <matheusorienrac@gmail.com>
2021-01-12 23:08:21 +0000

    Solution to Project Euler Problem 10

--- project_euler_10.py
+++ project_euler_10.py
@@ -1,7 +1,7 @@
 # The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
-
 # Find the sum of all the primes below two million.
 
+import time
 def is_prime(number, primes_list):
     """
     Checks if a certain number is a prime number using a list of prime numbers
@@ -10,17 +10,27 @@ def is_prime(number, primes_list):
     :param primes_list: list with all prime numbers smaller than number
     :return: true if prime, false if not
     """
+    if (number % 10 == 5):
+        return False
+
+    if primes_list[round(primes_list * 9 / 16 - 1)] > number // 2:
+        primes_list = primes_list[0:round(primes_list * 9 / 16 - 1)]
+    elif primes_list[round(primes_list * 5 / 8 - 1)] > number // 2:
+        primes_list = primes_list[0:round(primes_list * 5 / 8 - 1)]
+    elif primes_list[round(primes_list * 3 / 4 - 1)] > number // 2:
+        primes_list = primes_list[0:round(primes_list * 3 / 4 - 1)]
     for prime in primes_list:
-        if number % prime == 0:
-            return False
+            if number % prime == 0:
+                return False
     return True
 
-
-primes_list = [2]  # will use this to reduce amount of numbers to test in the
-# is_prime function
-Sum = 2  # starting with number 2 so the 'for' below can skip even numbers
-for i in range(3, 2000000, 2):
+start = time.time()
+primes_list = [2, 3, 5] # will use this to reduce amount of numbers to test in the
+Sum = 10  # starting with number 10 so the 'for' below can skip 2,3,5
+for i in range(7, 2000000, 2):
     if is_prime(i, primes_list):
         Sum += i
         primes_list.append(i)
-print(Sum)
+
+end = time.time()
+print(Sum, end - start)
